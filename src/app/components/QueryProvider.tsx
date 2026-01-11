4b61d63735c47540c9480a211f9f276b83f2532c
andrei_doroteo <andreitot13@gmail.com>
2026-01-10 21:53:44 -0800

    feat: Add QueryProvider component.

---
+++ src/app/components/QueryProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function QueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000, // 60 seconds
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export { QueryProvider };
